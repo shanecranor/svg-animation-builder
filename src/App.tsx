5eebe3c7ecb66f8fece10a09497a53b94bf08ada
shanecranor <shanecranor@gmail.com>
2024-06-09 05:26:16 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>stest</div>
+    </>
+  );
+}
+
+export default App;
