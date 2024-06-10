07addc238ec5e3f38b52d0912f803594ffe6a28d
shanecranor <shanecranor@gmail.com>
2024-06-10 09:30:03 -0500

    simple template working

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import "./App.css";
-
+import { test } from "./svg-temp/templates/templates";
 function App() {
   const [count, setCount] = useState(0);
+  const svg = new Blob([test], { type: "image/svg+xml" });
+  const svgUrl = URL.createObjectURL(svg);
 
   return (
     <>
-      <div>stest</div>
+      <div>{test}</div>
+      <img src={svgUrl}></img>
     </>
   );
 }
